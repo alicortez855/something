ce56788c1ad9266a290245c849002e71606d81b4
alicortez855 <alicortez6@gmail.com>
2025-06-17 22:26:33 +0000

    red

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 document.getElementById("greeting").textContent = "Hi there!"; 
 document.getElementById("h1").textContent = "The Header";
 
+let items = document.getElementsByClassName("redclass");
+for(let i = 0; i <items.length; i++)
+{
+    items[i].textContent = "Text change";
+}
+
+const header = document.querySelector("h1");
+console.log(header);
+const red = document.querySelectorAll(".redclass");
+console.log(red);
+document.getElementById("click").addEventListener("click",function()
+{
+    alert("CLICKED BUTTON!");
+});
+
 /*
 run this first: php -S localhost:3006 -t./
 */
